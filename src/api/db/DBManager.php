cf21b35a7b1cde7b00a7bba79031a88d90e9b377
0WiIIian0 <84929084+0WiIIian0@users.noreply.github.com>
2022-04-21 23:29:44 -0300

    signin implementation

--- src/api/db/DBManager.php
+++ src/api/db/DBManager.php
@@ -1,3 +1,20 @@
 <?php
+    <?php
 
+    class DB {
+        public $pdo;
+    
+        function __construct() {
+    
+            try {	
+                $this->pdo = new PDO("mysql:host=localhost;dbname=notes","root",""); 
+            } catch(PDOException $e) {
+                die('Failed to connect to local database.');
+            }
+    
+        }
+    
+    }
+    
+    ?>
 ?>
--- src/api/user/signin.php
+++ src/api/user/signin.php
@@ -1,3 +1,42 @@
 <?php
+    session_start();
+
+    include_once("../db/DBManager.php");
+	
+	$my_DB = new DB();	
+	
+	$pdo = $my_DB->pdo;
+
+	$email = $_POST['email'];
+	$password = $_POST['password'];
+    
+	$sql = "select * from users where email = :email";
+
+	$cmd = $pdo->prepare($sql);
+
+	$cmd->bindValue(":email", md5($email));
+	
+	$cmd->execute();
+
+    echo json_encode ($email,$password);
+    
+	if( $dados = $cmd->fetch(PDO::FETCH_ASSOC) )
+	{
+        if(password_verify($password,$dados['pass']))
+        {
+			$_SESSION['id']   = $dados['id'];
+			$_SESSION['user'] = $dados['user'];
+
+			//header("Location: ");
+        }
+        else
+        {
+        	//header("Location: ");
+        }
+	} 
+	else 
+	{        	
+		//header("Location: ");	
+	}
 
 ?>
