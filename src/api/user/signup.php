83c64099c722ad060d2ed01a07ecd851bc2939e8
0WiIIian0 <84929084+0WiIIian0@users.noreply.github.com>
2022-04-21 23:51:55 -0300

    signout and signup implementation

--- src/api/user/signup.php
+++ src/api/user/signup.php
@@ -1,3 +1,37 @@
 <?php
 
+    include_once("../db/DBManager.php");
+
+    $my_DB = new DB();	
+	
+	$pdo = $my_DB->pdo;
+
+	$sql = "insert into notes (nome,email,password) values (:nome, :email, :password)";
+
+	$cmd = $pdo->prepare($sql);
+
+	$name  = $_POST['name'];               
+	$email = $_POST['email'];     
+	$password  = $_POST['password'];
+
+	$cmd->bindValue(":nome"    , $nome);                    
+	$cmd->bindValue(":email"   , md5($email));         
+	$cmd->bindValue(":password"    , password_hash($password), PASSWORD_DEFAULT); 
+
+	$cmd->execute();
+
+    echo json_encode($name,$email,$password);
+    
+	if($dados = $cmd->fetch(PDO::FETCH_ASSOC))
+	{
+		echo json_encode(array(
+			'result' => 201
+		));
+	}
+	else
+	{
+		echo json_encode(array(
+			'result' => 500
+		));
+	}
 ?>
