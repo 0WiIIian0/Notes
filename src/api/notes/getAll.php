8e0cdffd5976eb3e2a6065aafcc2cdbb8a37a183
0WiIIian0 <84929084+0WiIIian0@users.noreply.github.com>
2022-04-22 22:14:58 -0300

    delete.php, get.php, getAll.php, post.php and update,php implementation

--- src/api/notes/getAll.php
+++ src/api/notes/getAll.php
@@ -1,3 +1,19 @@
 <?php
 
+    session_start();
+    include_once("../db/connectDB.php");
+
+    $my_DB = new DB();	
+	
+	$pdo = $my_DB->pdo;
+
+	$sql = "select id, title, content from notes where user = :user";
+
+	$cmd = $pdo->prepare($sql);
+	$cmd->bindValue(":user" , $_SESSION['id']);  
+
+	$cmd->execute();
+
+	echo json_encode($cmd->fetchAll(PDO::FETCH_CLASS));
+    
 ?>
--- src/api/notes/post.php
+++ src/api/notes/post.php
@@ -1,13 +1,39 @@
 <?php
 
-$title = $_POST['title'];
-$content = $_POST['content'];
+    session_start();
+    include_once("../db/DBManager.php");
 
-echo json_encode(
-  array(
-    'title' => $title,
-    'content' => $content
-  )
-);
+    $my_DB = new DB();	
+
+    $pdo = $my_DB->pdo;
+
+    $sql = "insert into notes (user,title,content) values (:user, :title, :content)";
+
+    $cmd = $pdo->prepare($sql);
+
+    $user  = $_SESSION['id'];                    
+    $title = $_POST['title'];     
+    $content  = $_POST['content'];
+
+    $cmd->bindValue(":user"    , $user);                    
+    $cmd->bindValue(":title"   , $title);         
+    $cmd->bindValue(":content"    , $content); 
+
+    $cmd->execute();
+    
+    json_encode($user,$title,$content);
+
+    if($dados = $cmd->fetch(PDO::FETCH_ASSOC))
+    {
+    echo json_encode(array(
+      'result' => 201
+    ));
+    }
+    else
+    {
+    echo json_encode(array(
+      'result' => 500
+    ));
+    }
 
 ?>
